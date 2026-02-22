108070f4b01d301a7c1137124b61977a7a369790
pedramktb <79080845+pedramktb@users.noreply.github.com>
2026-02-22 18:57:18 +0100

    fix: add drivers for lib version of cli

--- cli/cmd/lib/main.go
+++ cli/cmd/lib/main.go
@@ -18,6 +18,14 @@ import (
 	"unsafe"
 
 	"github.com/pedramktb/go-netx/cli/internal"
+	_ "github.com/pedramktb/go-netx/drivers/aesgcm"
+	_ "github.com/pedramktb/go-netx/drivers/dnst"
+	_ "github.com/pedramktb/go-netx/drivers/dtls"
+	_ "github.com/pedramktb/go-netx/drivers/dtlspsk"
+	_ "github.com/pedramktb/go-netx/drivers/ssh"
+	_ "github.com/pedramktb/go-netx/drivers/tls"
+	_ "github.com/pedramktb/go-netx/drivers/tlspsk"
+	_ "github.com/pedramktb/go-netx/drivers/utls"
 )
 
 func main() {}
