b5aeb7cf208f5933a5a721e94986fb8420c57151
pedramktb <79080845+pedramktb@users.noreply.github.com>
2026-02-28 19:28:27 +0100

    refactor: improve parameter handling in dnst registration and update connection creation

--- drivers/dnst/dnst.go
+++ drivers/dnst/dnst.go
@@ -3,6 +3,7 @@ package dnst
 import (
 	"fmt"
 	"net"
+	"strconv"
 
 	"github.com/pedramktb/go-netx"
 	dnstproto "github.com/pedramktb/go-netx/proto/dnst"
@@ -10,9 +11,27 @@ import (
 
 func init() {
 	netx.Register("dnst", func(params map[string]string, listener bool) (netx.Wrapper, error) {
-		domain := params["domain"]
+		var domain string
+		opts := []dnstproto.ServerOption{}
+		for key, value := range params {
+			switch key {
+			case "domain":
+				domain = value
+			case "maxw":
+				if !listener {
+					return netx.Wrapper{}, fmt.Errorf("dnst: max write parameter is only valid for listeners")
+				}
+				size, err := strconv.ParseUint(value, 10, 16)
+				if err != nil {
+					return netx.Wrapper{}, fmt.Errorf("dnst: invalid max write parameter %q: %w", value, err)
+				}
+				opts = append(opts, dnstproto.WithMaxWrite(uint16(size)))
+			default:
+				return netx.Wrapper{}, fmt.Errorf("dnst: unknown parameter %q", key)
+			}
+		}
 		if domain == "" {
-			return netx.Wrapper{}, fmt.Errorf("uri: missing dnst domain parameter")
+			return netx.Wrapper{}, fmt.Errorf("dnst: missing domain parameter")
 		}
 		if listener {
 			return netx.Wrapper{
@@ -20,7 +39,7 @@ func init() {
 				Params:   params,
 				Listener: listener,
 				ConnToTagged: func(c net.Conn) (netx.TaggedConn, error) {
-					return dnstproto.NewDNSTServerConn(c, domain), nil
+					return dnstproto.NewServerConn(c, domain, opts...), nil
 				}}, nil
 		}
 		return netx.Wrapper{
@@ -28,7 +47,7 @@ func init() {
 			Params:   params,
 			Listener: listener,
 			ConnToConn: func(c net.Conn) (net.Conn, error) {
-				return dnstproto.NewDNSTClientConn(c, domain), nil
+				return dnstproto.NewClientConn(c, domain), nil
 			}}, nil
 	})
 }
